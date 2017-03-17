10c4e0a1a0daf6f0a4c94c79fcf092a2b933403a
rrramiro <ramiro.sarmiento@gmail.com>
2017-03-17 00:25:32 +0000

    Remove reflexion

--- src/main/scala/fr/ramiro/scala/currency/package.scala
+++ src/main/scala/fr/ramiro/scala/currency/package.scala
@@ -3,6 +3,7 @@ package fr.ramiro.scala
 import scala.language.implicitConversions
 
 package object currency {
+  type Currency = Currency.Value
   type AmountType = BigDecimal
   type ConverterMethodType = PartialFunction[(Currency, Currency), AmountType]
   type ConfigurationType = CurrencyConversionConfiguration
